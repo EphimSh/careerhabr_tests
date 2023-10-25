84dff7e6de2ac9cceaaa823077a687df1f9788bb
EphimSh <ephimizm@gmail.com>
2023-10-25 19:12:36 +0300

    updated: specifications added

--- src/test/java/careerhabr/api/spec/Specification.java
+++ src/test/java/careerhabr/api/spec/Specification.java
@@ -1,12 +1,12 @@
 package careerhabr.api.spec;
 
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static careerhabr.helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.with;
 
 
@@ -19,7 +19,15 @@ public class Specification {
             .contentType(ContentType.JSON);
 
     public static ResponseSpecification getVacanciesListWithStatusCode200 = new ResponseSpecBuilder()
-            .log(LogDetail.STATUS)
             .expectStatusCode(200)
+            .expectBody(matchesJsonSchemaInClasspath("schemas/front-end-api-vacancies-schema.json"))
+            .build();
+    public static ResponseSpecification getResumesListWithStatusCode200 = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .expectBody(matchesJsonSchemaInClasspath("schemas/front-end-api-resumes-response-schema.json"))
+            .build();
+
+    public static ResponseSpecification postVacanciesListWithStatusCode422 = new ResponseSpecBuilder()
+            .expectStatusCode(422)
             .build();
 }
